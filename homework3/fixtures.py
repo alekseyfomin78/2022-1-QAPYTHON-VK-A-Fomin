c393e4421d3c00ebe64e934dc7902e97e76fc21f
alfomin11 <fominal11@mail.ru>
2022-04-07 21:13:35 +0300

    Commit. Update homework3

--- homework3/fixtures.py
+++ homework3/fixtures.py
@@ -4,8 +4,9 @@ import pytest
 
 
 @pytest.fixture(scope='session')
-def credentials():
-    with open('files/credentials.txt', 'r') as f:
+def credentials(repo_root):
+    file = os.path.join(repo_root, 'files', 'credentials.txt')
+    with open(file, 'r') as f:
         user = f.readline().strip()
         password = f.readline().strip()
     return user, password
